8be8baeaf59ea18c2c262f7d948e440f9fd64bc7
Andre Dantas <andremoreirati@gmail.com>
2025-03-31 20:30:39 -0300

    Conhecendo Tipo de variaveis javascript

---
+++ javascript-basico/aula4/index.js
@@ -0,0 +1,11 @@
+let nome = 'Roberto'
+let numero = 0.253
+let usuarioVerificado = false
+
+let pessoa = null
+
+if(usuarioVerificado){
+    console.log("Usuário verificado")
+}else{
+    console.log("Usuário não verificado")
+}
