f157f99a4aab7b3e3157e6a8c9bd7a9eb67111df
Andre Dantas <andremoreirati@gmail.com>
2025-04-02 20:00:43 -0300

    aprendendo condicional se e senão

---
+++ javascript-basico/aula09/index.js
@@ -0,0 +1,9 @@
+let nomeFilme = 'Batman';
+
+if(nomeFilme === 'Vingadores'){
+    console.log('É o filme dos ' + nomeFilme);
+} else if(nomeFilme === 'Fast and Furious'){
+    console.log('É o filme ' + nomeFilme);
+}else {
+    console.log('É outro Filme')
+}
