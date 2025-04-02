23d6fd77e59570fb965bbd5074be038905c3852e
Andre Dantas <andremoreirati@gmail.com>
2025-04-02 19:45:10 -0300

    aprendendo laços while e do while

--- javascript-basico/aula08/index.js
+++ javascript-basico/aula08/index.js
@@ -9,3 +9,15 @@ while(a < b){
     console.log('Inclementando a variável a ' + a);
     a++;
 }
+
+//do {
+//bloco de execução}
+//while (condição)
+
+var i = 1;
+
+do {
+    console.log('entrou' + i);
+    i++;
+}
+while(i < 5);
