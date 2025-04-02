e25717ee45a6a243a9e44dc317c8f004f2ac4ee3
Andre Dantas <andremoreirati@gmail.com>
2025-04-02 20:31:14 -0300

    mexendo com operador ternario if

--- javascript-basico/aula09/index.js
+++ javascript-basico/aula09/index.js
@@ -7,3 +7,7 @@ if(nomeFilme === 'Vingadores'){
 }else {
     console.log('É outro Filme')
 }
+
+false ? console.log('Condição verdadeira') : console.log('Condição Falsa');
+
+nomeFilme === 'Vingadores' ? console.log('É o filme dos Vingadores') : console.log('É outro filme!');
