a797e848ed0dd4f3f1806a5f9edd5495296f309a
Andre Dantas <andremoreirati@gmail.com>
2025-04-01 21:33:32 -0300

    Aprendendo operadores matemáticos e estruturas de repetição e condição

---
+++ javascript-basico/aula06/index.js
@@ -0,0 +1,12 @@
+ let primeiroValor = 4;
+ let segundoValor = 5;
+
+ let somaTotal = primeiroValor + segundoValor;
+
+ console.log(somaTotal);
+
+ console.log(primeiroValor - segundoValor);
+ console.log(primeiroValor * segundoValor);
+ console.log(segundoValor / primeiroValor);
+
+ console.log(segundoValor % primeiroValor);
