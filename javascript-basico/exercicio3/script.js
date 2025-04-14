50faac0129703ef80cfdc96f8dc397ba2b06e097
Andre Dantas <andremoreirati@gmail.com>
2025-04-14 20:30:53 -0300

    resolução exercicios 3, 4  e 5

---
+++ javascript-basico/exercicio3/script.js
@@ -0,0 +1,8 @@
+
+let resposta = confirm("Seja Bem Vindo(a), Aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?");
+
+if (resposta === true) {
+    alert("Ótimo! Nós temos as melhores camas de toda a região!");
+} else if(resposta === false) {
+    alert("Que pena! Você parecia ser uma pessoa legal.");
+}
