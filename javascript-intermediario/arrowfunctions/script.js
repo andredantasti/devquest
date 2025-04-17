c57fc244f683c5addbec7636171ce0e42385009c
Andre Dantas <andremoreirati@gmail.com>
2025-04-17 16:12:46 -0300

    aprendendo funções anonimas e arrows functions

---
+++ javascript-intermediario/arrowfunctions/script.js
@@ -0,0 +1,8 @@
+let resultadoDaSoma = (numero1, numero2) => numero1 + numero2;
+console.log(resultadoDaSoma(1, 2));
+
+const incentivarQuester = nomeQuester =>{
+    console.log("Parabéns " + nomeQuester + ", Você chegou ao módulo de JS Intermediário.");
+}
+
+incentivarQuester("Joaquim");
