b386b9954bf4095031bc34ba0e21e7f6758d97a7
Andre Dantas <andremoreirati@gmail.com>
2025-04-17 20:58:40 -0300

    array multidimensional e console table

---
+++ javascript-intermediario/arrays-multidimensional/script.js
@@ -0,0 +1,7 @@
+let filmes = [
+  ["Vingadores", 4.5, true],
+  ["Vingadores: Guerra Infinita", 4.1, false, ["Tony Stark", 35, "Homem de Ferro"]],
+  ["Vingadores: Ultimato", 3.5, false],
+];
+
+console.table(filmes);
