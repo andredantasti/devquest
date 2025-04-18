91acc930519a9ff67228623329a4dfa602c790a9
Andre Dantas <andremoreirati@gmail.com>
2025-04-18 16:32:10 -0300

    entendendo a diferença entre array e objetos

---
+++ javascript-intermediario/diferenca-array-objetos/script.js
@@ -0,0 +1,19 @@
+let frutas = ["banana", "maca", "abacaxi", "laranja"];
+
+let videoGame = {
+    nome: "Xbox",
+    valor: 3000
+}
+
+let videoGame2 = {
+    nome: "PlayStation",
+    valor: 5000
+}
+
+//colocando objetos em um array
+
+let videoGames = [videoGame, videoGame2];
+
+console.log(videoGames);
+
+console.log(videoGames[0].nome);
