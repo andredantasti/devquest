dd88600b8f0fe1c7b0fb2b241c3d390b7e1dd051
Andre Dantas <andremoreirati@gmail.com>
2025-04-26 16:06:58 -0300

    concluindo exercicio com o que conseguimos fazer em java e css carrossel de imagens

---
+++ javascript-intermediario/exercicio5/script.js
@@ -0,0 +1,47 @@
+const btnEsquerda = document.querySelector(".btn-seta-esquerda");
+
+const btnDireita = document.querySelector(".btn-seta-direita");
+
+const esconderImagem = document.getElementsByClassName("esconder");
+
+let imagensDeFundo = document.getElementsByTagName("section");
+
+btnEsquerda.addEventListener("click", function () {
+  for (let i = 0; i <= 3; i++) {
+    if (imagensDeFundo[i].classList.contains("container1")) {
+      imagensDeFundo[i].classList.remove("container1");
+      imagensDeFundo[i].classList.add("container2");
+    } else if (imagensDeFundo[i].classList.contains("container2")) {
+      imagensDeFundo[i].classList.remove("container2");
+      imagensDeFundo[i].classList.add("container3");
+    } else if (imagensDeFundo[i].classList.contains("container3")) {
+      imagensDeFundo[i].classList.remove("container3");
+      imagensDeFundo[i].classList.add("container4");
+    } else if (imagensDeFundo[i].classList.contains("container4")) {
+      imagensDeFundo[i].classList.remove("container4");
+      imagensDeFundo[i].classList.add("container1");
+    }
+        
+     }
+     desativarBotaoEsquerdo();
+  }
+);
+
+btnDireita.addEventListener("click", function () {
+  for (let i = 0; i < imagensDeFundo.length; i++) {
+    if (imagensDeFundo[i].classList.contains("container1")) {
+      imagensDeFundo[i].classList.remove("container1");
+      imagensDeFundo[i].classList.add("container4");
+    } else if (imagensDeFundo[i].classList.contains("container2")) {
+      imagensDeFundo[i].classList.remove("container2");
+      imagensDeFundo[i].classList.add("container1");
+    } else if (imagensDeFundo[i].classList.contains("container3")) {
+      imagensDeFundo[i].classList.remove("container3");
+      imagensDeFundo[i].classList.add("container2");
+    } else if (imagensDeFundo[i].classList.contains("container4")) {
+      imagensDeFundo[i].classList.remove("container4");
+      imagensDeFundo[i].classList.add("container3");
+    }
+  }
+});
+
