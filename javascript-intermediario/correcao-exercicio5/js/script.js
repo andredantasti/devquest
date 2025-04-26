d217583765573cc893b959d199b0b5736e7096e0
Andre Dantas <andremoreirati@gmail.com>
2025-04-26 19:42:54 -0300

    correcao exercicio 5 - slider carrosel imagem

---
+++ javascript-intermediario/correcao-exercicio5/js/script.js
@@ -0,0 +1,58 @@
+const imagens = document.querySelectorAll(".slide");
+console.log(imagens);
+
+const setaVoltar = document.getElementById("seta-voltar");
+const setaAvancar = document.getElementById("seta-avancar");
+
+let imagemAtual = 0;
+
+setaAvancar.addEventListener("click", function () {
+  if (imagemAtual === imagens.length - 1) {
+    setaAvancar.style.opacity = 0.6;
+    setaAvancar.disabled = true;
+    return;
+  }
+
+  esconderImagemAberta();
+
+  imagemAtual++;
+
+  imagens[imagemAtual].classList.add("mostrar");
+
+  mostrarOuEsconderSetas();
+});
+
+setaVoltar.addEventListener("click", function () {
+  if (imagemAtual === 0) {
+    return;
+  }
+
+  esconderImagemAberta();
+
+  imagemAtual--;
+
+  imagens[imagemAtual].classList.add("mostrar");
+
+  mostrarOuEsconderSetas();
+});
+function esconderImagemAberta() {
+  const imagemAberta = document.querySelector(".mostrar");
+  imagemAberta.classList.remove("mostrar");
+}
+
+function mostrarOuEsconderSetas() {
+  const naoEhAPrimeiraImagem = imagemAtual !== 0;
+  if (naoEhAPrimeiraImagem) {
+    setaVoltar.classList.remove("opacidade");
+  } else {
+    setaVoltar.classList.add("opacidade");
+  }
+
+  const chegouNaUltimaImagem =
+    imagemAtual == !0 && imagemAtual === imagens.length - 1;
+  if (chegouNaUltimaImagem) {
+    setaAvancar.classList.add("opacidade");
+  } else {
+    setaAvancar.classList.remove("opacidade");
+  }
+}
