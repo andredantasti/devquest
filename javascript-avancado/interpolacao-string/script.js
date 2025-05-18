a14614c79ddac5a39ffba6353ae6f6fabf7c4c05
Andre Dantas <andremoreirati@gmail.com>
2025-05-18 16:39:15 -0300

    interpolação de string

---
+++ javascript-avancado/interpolacao-string/script.js
@@ -0,0 +1,8 @@
+
+const imprimirInformacoesPessoa = (primeiroNome, ultimoNome, Idade) => {
+    return `Primeiro Nome: ${primeiroNome} Último Nome: ${ultimoNome} Idade: ${Idade}`;
+}
+console.log(imprimirInformacoesPessoa("Roberto", "Dias", 33));
+console.log(`${imprimirInformacoesPessoa("Andre", "Dantas", 38)}, ele é um Desenvolvedor Frontend Junior`)
+
+console.log(`Soma ${10 + 30}`)
